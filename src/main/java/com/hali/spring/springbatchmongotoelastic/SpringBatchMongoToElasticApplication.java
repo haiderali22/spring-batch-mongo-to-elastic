fda7152ea1592668a455c573dc7a4f2c92c7a7ce
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2023-03-11 18:56:57 +0500

    first commit

---
+++ src/main/java/com/hali/spring/springbatchmongotoelastic/SpringBatchMongoToElasticApplication.java
@@ -0,0 +1,13 @@
+package com.hali.spring.springbatchmongotoelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchMongoToElasticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchMongoToElasticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hali/spring/springbatchmongotoelastic/SpringBatchMongoToElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hali.spring.springbatchmongotoelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMongoToElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
